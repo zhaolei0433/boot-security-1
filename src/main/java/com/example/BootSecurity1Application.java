7d2392414e3596b039efddc6d5a7d6ea40543a87
zhaolei <zhaolei@ipanel.cn>
2020-03-09 16:48:53 +0800

    Initial commit

---
+++ src/main/java/com/example/BootSecurity1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSecurity1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootSecurity1Application.class, args);
+    }
+
+}
